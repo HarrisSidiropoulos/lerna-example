93867573817eaee8b7209231ab2361d811f8e558
Harris Sidiropoulos <theocharis.sidiropou@dazn.com>
2021-07-16 13:12:11 +0200

    test 3

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,7 +3,7 @@ import { TestComponent } from '@sidiropoulos/shared-components';
 function App() {
   return (
     <div className="App">
-      <TestComponent theme="secondary" />
+      <TestComponent theme="primary" />
     </div>
   );
 }
