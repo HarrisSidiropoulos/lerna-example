ed4541d0e62b0ac9f46182e039afa40b346e36e1
Harris Sidiropoulos <theocharis.sidiropou@dazn.com>
2021-07-16 14:08:16 +0200

    update tests

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const app = renderer.create(<App />).toJSON();
+
+  expect(app).toMatchInlineSnapshot(`
+    <div
+      className="App"
+    >
+      <div
+        className="test-component test-component-primary"
+        data-testid="test-component"
+      >
+        <h1
+          className="heading"
+        >
+          I'm the test component!
+        </h1>
+      </div>
+    </div>
+  `);
 });
