faa70b317d44cab729378365c08d2b81f36e3908
Harris Sidiropoulos <theocharis.sidiropou@dazn.com>
2021-07-21 19:26:41 +0200

    add test task to workspaces

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "./App";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   const app = renderer.create(<App />).toJSON();
 
   expect(app).toMatchInlineSnapshot(`
@@ -10,7 +10,7 @@ test("renders learn react link", () => {
       className="App"
     >
       <div
-        className="test-component test-component-primary"
+        className="test-component test-component-secondary"
         data-testid="test-component"
       >
         <h1
