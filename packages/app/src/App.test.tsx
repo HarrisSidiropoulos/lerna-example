f8e907567d6e89dfa4519a520eed7b865976dce1
Harris Sidiropoulos <theocharis.sidiropou@dazn.com>
2021-07-24 11:41:53 +0200

    feat: change theme (#14)
    * test feat
    * update snapshots

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
+jest.mock('@sidiropoulos/shared-components', () => ({
+  TestComponent: 'TestComponent',
+}));
+
 test('renders learn react link', () => {
   const app = renderer.create(<App />).toJSON();
 
@@ -9,16 +13,9 @@ test('renders learn react link', () => {
     <div
       className="App"
     >
-      <div
-        className="test-component test-component-secondary"
-        data-testid="test-component"
-      >
-        <h1
-          className="heading"
-        >
-          I'm the test component!
-        </h1>
-      </div>
+      <TestComponent
+        theme="primary"
+      />
     </div>
   `);
 });
